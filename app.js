7ed40ff0efb8cd1fe7a7c0cd36adcdd9ebbe8893
AbdullaDsouky220 <100207558+AbdullaDsouky220@users.noreply.github.com>
2022-05-05 12:56:56 +0200

    gg

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ console.log(images);
 images.addEventListener("click", (e) => {
   console.log(window.location.host,e.target.src)
   transform.src = e.target.src;
-  if (e.target.src == `https://${window.location.host}/iphone-project/photos/0.png`) {
+  if (e.target.src == `${window.location.href}photos/0.png`) {
     document.body.style.backgroundColor = "#e4bf5d";
   }
-  if (e.target.src == `https://${window.location.host}/iphone-project/photos/1.png`) {
+  if (e.target.src == `${window.location.href}photos/1.png`) {
     document.body.style.backgroundColor = "#94acc4";
   }
-  if (e.target.src == `https://${window.location.host}/iphone-project/photos/2.png`) {
+  if (e.target.src == `${window.location.href}photos/2.png`) {
     document.body.style.backgroundColor = "#464541";
   }
-  if (e.target.src == `https://${window.location.host}/iphone-project/photos/3.png`) {
+  if (e.target.src == `${window.location.href}photos/3.png`) {
     document.body.style.backgroundColor = "#e2ceb3";
   }
-  if (e.target.src == `https://${window.location.host}/iphone-project/photos/4.png`) {
+  if (e.target.src == `${window.location.href}photos/4.png`) {
     document.body.style.backgroundColor = "#e60000";
   }
 });
